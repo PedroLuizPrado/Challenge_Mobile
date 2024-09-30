2968591776155e920c195dc324b2703884ae34c5
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-30 17:23:54 -0300

    Telas

---
+++ app/src/main/java/com/example/odontoprevaap/FaceTeethPag.kt
@@ -0,0 +1,16 @@
+package com.example.odontoprevaap
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class FaceTeethPag : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_face_teeth_pag)
+
+    }
+}
