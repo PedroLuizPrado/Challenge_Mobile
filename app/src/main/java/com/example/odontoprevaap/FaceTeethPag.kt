6142385163e4c5442539ee10262053874fc762ee
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-30 17:31:29 -0300

    funções

--- app/src/main/java/com/example/odontoprevaap/FaceTeethPag.kt
+++ app/src/main/java/com/example/odontoprevaap/FaceTeethPag.kt
@@ -1,16 +1,31 @@
 package com.example.odontoprevaap
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class FaceTeethPag : AppCompatActivity() {
+
+    lateinit var btnVoltar: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_face_teeth_pag)
+
+
+        btnVoltar= findViewById(R.id.btnVoltar)
+        // Define um listener de clique para o botão
+        btnVoltar.setOnClickListener {
+            // Cria uma intenção para navegar para a TelaDois
+            val NavegarTelaInicial = Intent(this, MainActivity::class.java)
+            // Inicia a atividade TelaDois
+            startActivity(NavegarTelaInicial)
+        }
+
 
     }
 }
--- app/src/main/java/com/example/odontoprevaap/MainActivity.kt
+++ app/src/main/java/com/example/odontoprevaap/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.example.odontoprevaap
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var buttonFaceTeeth: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+      buttonFaceTeeth= findViewById(R.id.btnFaceTeeth)
+        // Define um listener de clique para o botão
+        buttonFaceTeeth.setOnClickListener {
+            // Cria uma intenção para navegar para a TelaDois
+            val NavegarTelaFaceTeeth = Intent(this, FaceTeethPag::class.java)
+            // Inicia a atividade TelaDois
+            startActivity(NavegarTelaFaceTeeth)
+        }
 
     }
 }
