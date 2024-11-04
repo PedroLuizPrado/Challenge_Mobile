62b29950c74b5d08b97fdd861dcbfcf131042dea
PedroLuizPrado <pedrolpsp2@gmail.com>
2024-11-04 11:07:27 -0300

    imagens e conexão firebase

--- Sprint02/app/src/main/java/com/example/sprint02/Recompensa.kt
+++ Sprint02/app/src/main/java/com/example/sprint02/Recompensa.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Task : AppCompatActivity() {
+class Recompensa : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_task)
+        setContentView(R.layout.activity_recompensa)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.cadastro)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
