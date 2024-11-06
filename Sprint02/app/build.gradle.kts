0d52b04b322a12cc23b810d6f02702e97b736fe7
PedroLuizPrado <142911409+PedroLuizPrado@users.noreply.github.com>
2024-11-06 17:00:47 -0300

    pagina login botoes e editext

--- Sprint02/app/build.gradle.kts
+++ Sprint02/app/build.gradle.kts
@@ -38,11 +38,21 @@ android {
 
 dependencies {
 
+    //Auntenticação
+    implementation("com.google.firebase:firebase-auth-ktx")
+
+    //Banco de dados
+    implementation("com.google.firebase:firebase-firestore-ktx")
+
+    //Armazenamento
+    implementation("com.google.firebase:firebase-storage-ktx")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
